07c054596aad4068d12d42dba94d6eb73c1e0785
shaoyuan1943@gmail.com <shaoyuan1943@gmail.com@dc05bb75-6a76-6f81-666c-f4466c85d6fd>
2013-07-27 16:13:50 +0000

    1、给DuiPcMgr添加日志模块，log4cplus_static作为DuiPcMgr的引用项目。日志宏在log.h中。写日志的方法参见main.cpp。 2、由于log4cplus_static项目的中间文件过大，请在commit的时候确定项目编译通过之后清理整个解决方案之后commit.

--- DuiPcMgr/main.cpp
+++ DuiPcMgr/main.cpp
@@ -1,8 +1,29 @@
 #include "stdafx.h"
 #include "UI/MainWnd.h"
+#include "log.h"
+#include <iostream>
+#include <string>
+using namespace log4cplus;
+using namespace log4cplus::helpers;
+
+
+DECLARE_SUBLOGITEM(DuiPcMgr_WinMain);
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int nCmdShow)
 {
+	std::locale::global(std::locale("chs"));
+	log4cplus::PropertyConfigurator::doConfigure(L".\\config\\log4cplus.cfg");
+	//日志的几种写法，还有LOG_DEBUG(),LOG_ERROR，具体的可在log.h中参看
+	LOG_TRACE_METHOD();
+	LOG_INFO(L"日志初始化成功......");
+	int i = 1024;
+	LOG_INFO(L"试试<<的作用1:"<< i);
+	std::wstring str = L"shaoyuan";
+	std::wstring str2 = L"少原";
+	LOG_INFO(L"试试<<的作用2:"<< str.c_str());
+	LOG_INFO(L"试试<<的作用3:"<<str2.c_str());
+
+
 	CPaintManagerUI::SetInstance(hInstance);
 
 	HRESULT Hr = ::CoInitialize(NULL);
