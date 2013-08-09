c22004438eaa8e0a1a52da6a83b791429249c877
daviyang35@gmail.com <daviyang35@gmail.com@dc05bb75-6a76-6f81-666c-f4466c85d6fd>
2013-08-09 15:26:58 +0000

    替换日志类，线程安全没有内存泄漏

--- trunk/DuiPcMgr/main.cpp
+++ trunk/DuiPcMgr/main.cpp
@@ -1,29 +1,11 @@
 #include "stdafx.h"
 #include "UI/MainWnd.h"
-#include "log.h"
-#include <iostream>
-#include <string>
-using namespace log4cplus;
-using namespace log4cplus::helpers;
 
-
-DECLARE_SUBLOGITEM(DuiPcMgr_WinMain);
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int nCmdShow)
 {
-	std::locale::global(std::locale("chs"));
-	log4cplus::PropertyConfigurator::doConfigure(L".\\log4cplus.cfg");
-	//日志的几种写法，还有LOG_DEBUG(),LOG_ERROR(),LOG_WARN()具体的可在log.h中参看
-	LOG_TRACE_METHOD();
-	LOG_INFO(L"日志初始化成功......");
-	int i = 1024;
-	LOG_INFO(L"试试<<的作用1:"<< i);
-	std::wstring str = L"shaoyuan";
-	std::wstring str2 = L"少原";
-	LOG_INFO(L"试试<<的作用2:"<< str.c_str());
-	LOG_INFO(L"试试<<的作用3:"<<str2.c_str());
-
-
+	g_logger.InitLogger();
+	g_logger.Info(_T("应用启动"));
 	CPaintManagerUI::SetInstance(hInstance);
 
 	HRESULT Hr = ::CoInitialize(NULL);
@@ -41,6 +23,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*l
 	CPaintManagerUI::MessageLoop();
 
 	::CoUninitialize();
+	g_logger.Info(_T("应用退出"));
 	return 0;
 }
 
